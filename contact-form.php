f9533eb874dd1a107ccd0d25a163749689bb95cd
Elsie Puig <epuigsant@gmail.com>
2016-11-30 20:29:48 -0800

    first commit

---
+++ contact-form.php
@@ -0,0 +1,35 @@
+<?php
+
+$to      = "epuigsant@gmail.com";
+$subject = "New Contact Form Submission";
+$message = trim(stripslashes($_POST['Message']));
+$from    = trim(stripslashes($_POST['Email']));
+$name    = trim(stripslashes($_POST['Name']));
+
+$validationOK = true;
+
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+$body  = "";
+$body .= "Name: ";
+$body .= $name;
+$body .= "\n";
+$body .= "Email: ";
+$body .= $from;
+$body .= "\n";
+$body .= "Message: ";
+$body .= $message;
+$body .= "\n";
+
+$success = mail($to, $subject, $body, "From: <$from>");
+
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=thank-you.php\">";
+} else {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+}
+
+?>
